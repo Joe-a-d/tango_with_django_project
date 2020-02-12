c0def33e28e51a43de915832f306526e6cfb5792
Joe-a-d <2334590D@student.gla.ac.uk>
2020-02-12 16:59:15 +0000

    finish 7 ; tests OK

--- rango/forms.py
+++ rango/forms.py
@@ -12,21 +12,21 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ('name',)
 
-    class PageForm(forms.ModelForm):
-        title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-        url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
-        views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
-        class Meta:
-            model = Page
-            exclude = ('category',)
-        
-        class clean(self):
-            cleaned_data = self.cleaned_data
-            url = cleaned_data.get('url')
+    class Meta:
+        model = Page
+        exclude = ('category',)
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
 
-            if url and not url.startswith('http://'):
-                url = f'http://{url}'
-                cleaned_data['url'] = url
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
 
-            return cleaned_data
+        return cleaned_data
