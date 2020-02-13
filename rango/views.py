79284029c9c617aacf5159a1a55a45897ce7d43d
Joe-a-d <2334590D@student.gla.ac.uk>
2020-02-13 20:20:41 +0000

    finish 10; tests OK

--- rango/views.py
+++ rango/views.py
@@ -5,6 +5,7 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
     # query objects sorted
@@ -16,11 +17,19 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    response = render(request, 'rango/index.html', context=context_dict)
+
+    return response
 
 def about(request):
+    visitor_cookie_handler(request)
+
     context_dict = {'boldmessage' : 'This tutorial has been put together by jd.'}
-    return render(request, 'rango/about.html', context=context_dict) 
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict) 
+
+    return response
 
 def show_category(request, category_name_slug):
     context_dict={}
@@ -135,3 +144,23 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
